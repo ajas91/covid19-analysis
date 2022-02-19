8efc1027d6dde7498bf4ce30c5ec9d22c4ffd5b7
Ali Sultan <alisultan@Alis-MacBook-Pro.local>
2022-02-19 22:28:21 +0400

    add commas to numbers

--- DashboardCovid/static/js/functions.js
+++ DashboardCovid/static/js/functions.js
@@ -2,7 +2,12 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-var val = parseInt($('.number').text());
-//Use the code in the answer above to replace the commas.
-val = numberWithCommas(val);
-$('.number').text(val);
+// var val = parseInt($('.number').text());
+// //Use the code in the answer above to replace the commas.
+// val = numberWithCommas(val);
+// $('.number').text(val);
+numberElementsLength = document.getElementsByClassName("number").length;
+for (let i = 0; i < numberElementsLength; i++) {
+    numberElement = document.getElementsByClassName("number")[i].innerHTML;
+    document.getElementsByClassName("number")[i].innerHTML = numberWithCommas(numberElement);
+  }
