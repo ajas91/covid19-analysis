edc1feae1b8b8ca5bc00e551cd22a3003f52aedc
Ali Sultan <ajasultan@gmail.com>
2022-02-20 12:47:15 +0400

    add control.py for firstUI app

---
+++ DashboardCovid/firstUI/control.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def readData():
+    '''
+    Read COVID 19 Dataset and returns to df
+    1: data read
+    2: dataframe with countries only
+    '''
+
+    data = pd.read_csv('data/owid-covid-data.csv')
+    data.loc[data['location']=='Israel','location']='Palestine' 
+    dataWithoutWorld = data.copy()
+    dataWithoutWorld = dataWithoutWorld.loc[~(dataWithoutWorld['continent'].isna())].groupby('location').tail(1) \
+                        .sort_values(by='total_cases',ascending=True).dropna(subset=['total_cases'])
+    dataWithoutWorld['cases_density'] = dataWithoutWorld['total_cases']/dataWithoutWorld['population']
+    dataDate = dataWithoutWorld.iloc[0]['date']
+
+    return data, dataWithoutWorld, dataDate
